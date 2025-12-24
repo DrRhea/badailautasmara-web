b173c5f33f8490fb7b09b89bd6604b608dbd3758
Arya Jagadditha <czajr13@gmail.com>
2025-12-25 01:59:36 +0700

    Rapikan nama file WhatsApp Image, tambahkan ke galeri, dan tukar urutan: B23 ARENA-20 jadi foto pertama

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 
 // Array untuk foto galeri
 const galleryImages = [
-  { src: "/images/gallery/gallery-10.png", alt: "Team Photo - JOGO" },
   { src: "/images/gallery/@B23 ARENA-20.jpg", alt: "B23 Arena Group Photo" },
+  { src: "/images/gallery/gallery-10.png", alt: "Team Photo - JOGO" },
   { src: "/images/gallery/@B23 ARENA-616.jpg", alt: "B23 Arena Team Photo" },
   { src: "/images/gallery/gallery-1.png", alt: "JOGO Training Session" },
   { src: "/images/gallery/gallery-3.png", alt: "Football Match Action" },
@@ -20,6 +20,9 @@ const galleryImages = [
   { src: "/images/gallery/@B23 ARENA-582.jpg", alt: "B23 Arena Training Activity" },
   { src: "/images/gallery/@B23 ARENA-699.jpg", alt: "B23 Arena Football Session" },
   { src: "/images/gallery/@B23 ARENA-772.jpg", alt: "B23 Arena Training Moment" },
+  { src: "/images/gallery/gallery-whatsapp-1.jpg", alt: "JOGO Training Moment" },
+  { src: "/images/gallery/gallery-whatsapp-2.jpg", alt: "Football Training Session" },
+  { src: "/images/gallery/gallery-whatsapp-3.jpg", alt: "Youth Football Training" },
 ];
 
 export default function GallerySection() {
